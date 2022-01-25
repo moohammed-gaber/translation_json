bdf34684ddce42c55a5cc2f973c0511d43830124
moohammed-gaber <moohammed.gaber@gmail.com>
2022-01-25 16:59:54 +0200

    Initial commit

---
+++ lib/generated/locales.g.dart
@@ -0,0 +1,466 @@
+// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
+
+// ignore_for_file: lines_longer_than_80_chars
+// ignore: avoid_classes_with_only_static_members
+class AppTranslation {
+  static Map<String, Map<String, String>> translations = {
+    'ar': Locales.ar,
+    'en': Locales.en,
+  };
+}
+
+class LocaleKeys {
+  LocaleKeys._();
+  static const home = 'home';
+  static const search = 'search';
+  static const categories = 'categories';
+  static const profile = 'profile';
+  static const editYourInfo = 'editYourInfo';
+  static const orders = 'orders';
+  static const mail = 'mail';
+  static const call = 'call';
+  static const wishlist = 'wishlist';
+  static const logout = 'logout';
+  static const searchHint = 'searchHint';
+  static const emptySearch = 'emptySearch';
+  static const darkTheme = 'darkTheme';
+  static const more_like_this = 'more_like_this';
+  static const customerReviews = 'customerReviews';
+  static const yourRating = 'yourRating';
+  static const rateProduct = 'rateProduct';
+  static const name = 'name';
+  static const email = 'email';
+  static const comment = 'comment';
+  static const submit = 'submit';
+  static const review = 'review';
+  static const masterPiece = 'masterPiece';
+  static const firstName = 'firstName';
+  static const lastName = 'lastName';
+  static const phone = 'phone';
+  static const address1 = 'address1';
+  static const address2 = 'address2';
+  static const address = 'address';
+  static const addAddress = 'addAddress';
+  static const productsDetails = 'productsDetails';
+  static const userDetails = 'userDetails';
+  static const orderDetails = 'orderDetails';
+  static const orderDate = 'orderDate';
+  static const orderStatus = 'orderStatus';
+  static const addresses = 'addresses';
+  static const favourite = 'favourite';
+  static const settings = 'settings';
+  static const contactUs = 'contactUs';
+  static const aboutUs = 'aboutUs';
+  static const city = 'city';
+  static const state = 'state';
+  static const country = 'country';
+  static const company = 'company';
+  static const postCode = 'postCode';
+  static const skip = 'skip';
+  static const splashTitle1 = 'splashTitle1';
+  static const splashSubtitle1 = 'splashSubtitle1';
+  static const splashTitle2 = 'splashTitle2';
+  static const splashSubtitle2 = 'splashSubtitle2';
+  static const splashTitle3 = 'splashTitle3';
+  static const splashSubtitle3 = 'splashSubtitle3';
+  static const splashTitle4 = 'splashTitle4';
+  static const continueToApp = 'continueToApp';
+  static const login = 'login';
+  static const register = 'register';
+  static const forgetPassword = 'forgetPassword';
+  static const emailOrUserName = 'emailOrUserName';
+  static const sendCode = 'sendCode';
+  static const sendingCode = 'sendingCode';
+  static const password = 'password';
+  static const resetPassword = 'resetPassword';
+  static const code = 'code';
+  static const emptyCart = 'emptyCart';
+  static const cart = 'cart';
+  static const placeOrder = 'placeOrder';
+  static const payment = 'payment';
+  static const egp = 'egp';
+  static const shipping = 'shipping';
+  static const inStock = 'inStock';
+  static const requiredValidationMessage = 'requiredValidationMessage';
+  static const emailValidationMessage = 'emailValidationMessage';
+  static const passwordValidationMessage = 'passwordValidationMessage';
+  static const lengthTwoValidationMessage = 'lengthTwoValidationMessage';
+  static const lengthValidationMessage = 'lengthValidationMessage';
+  static const phoneValidationMessage = 'phoneValidationMessage';
+  static const loginAndRegister = 'loginAndRegister';
+  static const chatUs = 'chatUs';
+  static const kwd = 'kwd';
+  static const byClicking = 'byClicking';
+  static const add = 'add';
+  static const removeFromCart = 'removeFromCart';
+  static const messages_EditedMessage = 'messages_EditedMessage';
+  static const messages_logoutMessage = 'messages_logoutMessage';
+  static const messages_somethingWrong = 'messages_somethingWrong';
+  static const messages_order = 'messages_order';
+  static const messages_location = 'messages_location';
+  static const messages_cancelOrder = 'messages_cancelOrder';
+  static const chooseLanguage = 'chooseLanguage';
+  static const english = 'english';
+  static const arabic = 'arabic';
+  static const quantity = 'quantity';
+  static const piece = 'piece';
+  static const language = 'language';
+  static const appVersion = 'appVersion';
+  static const total = 'total';
+  static const outOfStock = 'outOfStock';
+  static const emailAlreadyUsed = 'emailAlreadyUsed';
+  static const buyNow = 'buyNow';
+  static const chooseAddress = 'chooseAddress';
+  static const street = 'street';
+  static const buildingNo = 'buildingNo';
+  static const default_one = 'default_one';
+  static const pickYourLocation = 'pickYourLocation';
+  static const editAddress = 'editAddress';
+  static const selectCountry = 'selectCountry';
+  static const government = 'government';
+  static const selectGovernment = 'selectGovernment';
+  static const here = 'here';
+  static const orderNumber = 'orderNumber';
+  static const cash = 'cash';
+  static const emptyCities = 'emptyCities';
+  static const aToZ = 'aToZ';
+  static const zToA = 'zToA';
+  static const highToLow = 'highToLow';
+  static const lowToHigh = 'lowToHigh';
+  static const flashSale = 'flashSale';
+  static const topSeller = 'topSeller';
+  static const sortBy = 'sortBy';
+  static const mostLiked = 'mostLiked';
+  static const special = 'special';
+  static const areYouSure = 'areYouSure';
+  static const yes = 'yes';
+  static const no = 'no';
+  static const confirm = 'confirm';
+  static const verificationCode = 'verificationCode';
+  static const weSentSms = 'weSentSms';
+  static const resend = 'resend';
+  static const enterCode = 'enterCode';
+  static const invalidCode = 'invalidCode';
+  static const weBlockedNumber = 'weBlockedNumber';
+  static const codeExpired = 'codeExpired';
+  static const tooManyRequest = 'tooManyRequest';
+  static const invalidPhoneNumber = 'invalidPhoneNumber';
+  static const phoneOrEmail = 'phoneOrEmail';
+  static const phoneAlreadyUsed = 'phoneAlreadyUsed';
+  static const ratingBased = 'ratingBased';
+  static const userReviews = 'userReviews';
+  static const cancelOrder = 'cancelOrder';
+  static const returnOrder = 'returnOrder';
+  static const orderReturned = 'orderReturned';
+  static const orderCanceled = 'orderCanceled';
+  static const pendingOrder = 'pendingOrder';
+  static const orderCompleted = 'orderCompleted';
+  static const Hello_world = 'Hello_world';
+}
+
+class Locales {
+  static const ar = {
+    'home': 'الصفحة الرئيسية',
+    'search': 'بحث',
+    'categories': 'فئات',
+    'profile': 'الملف الشخصي',
+    'editYourInfo': 'تعديل المعلومات الخاصة بك',
+    'orders': 'الطلبات',
+    'mail': 'ارسل لنا عبر البريد الإلكتروني',
+    'call': 'اتصل بنا',
+    'wishlist': 'المفضلة',
+    'logout': 'تسجيل خروج',
+    'searchHint': 'ما الذي تبحث عنه ؟',
+    'emptySearch': 'لم نعثر على ما تبحث عنه جرب كلمة أخرى .',
+    'darkTheme': 'مظهر داكن',
+    'more_like_this': 'المزيد مثل هذا',
+    'customerReviews': 'آراء العملاء',
+    'yourRating': 'تقييمك',
+    'rateProduct': 'قيم المنتج الآن',
+    'name': 'اسم',
+    'email': 'بريد الالكتروني',
+    'comment': 'تعليق',
+    'submit': 'تأكيد',
+    'review': 'التقييم',
+    'masterPiece': 'ماستربيس',
+    'firstName': 'الاسم الأول',
+    'lastName': 'الكنية',
+    'phone': 'هاتف',
+    'address1': 'العنوان 1',
+    'address2': 'العنوان 2',
+    'address': 'العنوان',
+    'addAddress': 'اضف عنوان جديد',
+    'productsDetails': 'بيانات المنتجات',
+    'userDetails': 'بيانات المستخدم',
+    'orderDetails': 'بيانات الطلب',
+    'orderDate': 'تاريخ الطلب',
+    'orderStatus': 'حالة الطلب',
+    'addresses': 'العناوين',
+    'favourite': 'المفضلة',
+    'settings': 'الإعدادات',
+    'contactUs': 'تواصل معنا',
+    'aboutUs': 'عننا',
+    'city': 'مدينة',
+    'state': 'ولاية',
+    'country': 'دولة',
+    'company': 'شركة',
+    'postCode': 'الرمز البريدي',
+    'skip': 'تخطي',
+    'splashTitle1': 'احصل على أفضل العروض',
+    'splashSubtitle1': 'لا تفوتك صفقات اليوم نود أن نرسل لك عروض رائعة كل يوم',
+    'splashTitle2': 'توريد المصنع مباشرة',
+    'splashSubtitle2': 'يتم توفير معظم المنتج مباشرة من المصنع',
+    'splashTitle3': 'ابق على اطلاع على المنتجات',
+    'splashSubtitle3': 'الحصول على إشعار للمنتجات والتحديثات الجديدة',
+    'splashTitle4': 'جاهز للذهاب',
+    'continueToApp': 'استمر',
+    'login': 'تسجيل الدخول',
+    'register': 'التسجيل',
+    'forgetPassword': 'نسيت كلمة المرور؟',
+    'emailOrUserName': 'البريد الإلكتروني / اسم المستخدم',
+    'sendCode': 'أرسل الكود',
+    'sendingCode': 'جاري ارسال الكود',
+    'password': 'كلمه السر',
+    'resetPassword': 'إعادة تعيين كلمة المرور',
+    'code': 'الكود',
+    'emptyCart': 'السلة فارغة ,ابدا فى اضافة المنتجات الآن .',
+    'cart': 'عربة التسوق',
+    'placeOrder': 'عمل طلب',
+    'payment': 'وسيلة الدفع',
+    'egp': 'ج.م',
+    'shipping': 'وسيلة الشحن',
+    'inStock': 'متوفر',
+    'requiredValidationMessage': 'هذا الحقل لا يمكن ان يكون فارغا',
+    'emailValidationMessage': 'هذا الحقل يتطلب عنوان بريد إلكتروني صالح',
+    'passwordValidationMessage': 'يجب أن يكون طول القيمة أكبر من أو يساوي 6',
+    'lengthTwoValidationMessage': 'يجب أن يكون طول القيمة أكبر من أو يساوي 2',
+    'lengthValidationMessage':
+        'يجب أن يكون طول القيمة أكبر من أو يساوي @length',
+    'phoneValidationMessage': 'يتطلب هذا الحقل رقم هاتف صالح',
+    'loginAndRegister': 'تسجيل الدخول والتسجيل',
+    'chatUs': 'دردش معنا',
+    'kwd': 'د.ك',
+    'byClicking': 'من خلال النقر يمكنك تقييم هذا المنتج',
+    'add': 'اضف للسلة',
+    'removeFromCart': 'ازل من السلة',
+    'messages_EditedMessage': 'نجح تحرير المعلومات الخاصة بك.',
+    'messages_logoutMessage': 'تم تسجيل الخروج بنجاح.',
+    'messages_somethingWrong': 'هناك خطأ ما.',
+    'messages_order': 'تم إجراء الطلب بنجاح',
+    'messages_location': 'قم بتحديد عنوانك على الخريطة اولا',
+    'messages_cancelOrder': 'تم الغاء الطلب بنجاح',
+    'chooseLanguage': 'اختر لغتك',
+    'english': 'إنجليزي',
+    'arabic': 'عربي',
+    'quantity': 'الكمية',
+    'piece': 'قطعة',
+    'language': 'اللغة',
+    'appVersion': 'نسخة التطبيق',
+    'total': 'المجموع',
+    'outOfStock': 'غير متوفر',
+    'emailAlreadyUsed': 'البريد الالكتروني مستخدم من قبل جرب بريد اخر',
+    'buyNow': 'اشتري الآن',
+    'chooseAddress': 'اختر العنوان',
+    'street': 'الشارع',
+    'buildingNo': 'رقم المبني',
+    'default_one': 'الافتراضي',
+    'pickYourLocation': 'حدد مكانك',
+    'editAddress': 'عدل العنوان',
+    'selectCountry': 'اختر بلدك',
+    'government': 'المدينة',
+    'selectGovernment': 'اختر مدينتك',
+    'here': 'هنا',
+    'orderNumber': 'رقم الطلب',
+    'cash': 'الدفع عند الاستلام',
+    'emptyCities': 'لا يوجد مدن فى هذا البلد.',
+    'aToZ': 'أ - ي',
+    'zToA': 'ي - أ',
+    'highToLow': 'الاعلي للاقل',
+    'lowToHigh': 'الاقل للاعلي',
+    'flashSale': 'خصم',
+    'topSeller': 'البائعين المتميزين',
+    'sortBy': 'ترتيب حسب',
+    'mostLiked': 'الأكثر اعجاب',
+    'special': 'فريد',
+    'areYouSure': 'هل انت متاكد',
+    'yes': 'نعم',
+    'no': 'لا',
+    'confirm': 'تاكيد',
+    'verificationCode': 'الكود التأكيدي',
+    'weSentSms': 'نحن ارسلنا كود التأكيد على الرقم',
+    'resend': 'اعادة الارسال',
+    'enterCode': 'ادخل الكود',
+    'invalidCode': 'الكود خاطئ',
+    'weBlockedNumber':
+        'لقد حظرنا جميع الطلبات الواردة من هذا الجهاز لوجود نشاط غير عادي. حاول مرة أخرى لاحقًا',
+    'codeExpired': 'الكود المدحل صنتهي الصلاحية',
+    'tooManyRequest':
+        'هناك عدد كبير جدًا من الطلبات من هذا الرقم ، لذلك قمنا بحظره في الوقت الحالي',
+    'invalidPhoneNumber': 'رقم الهاتف غير صحيح',
+    'phoneOrEmail': 'البريد الالكتروني او الهاتف',
+    'phoneAlreadyUsed': 'هذا الرقم مستخدم من قبل',
+    'ratingBased': 'مبني على %s تقييمات',
+    'userReviews': 'تقييمات المستخدمين',
+    'cancelOrder': 'الغاء الطلب',
+    'returnOrder': 'استرجاع الطلب',
+    'orderReturned': 'تم تقديم طلب استرجاع',
+    'orderCanceled': 'تم تقديم طلب الغاء',
+    'pendingOrder': 'جاري تنفيذ الطلب',
+    'orderCompleted': 'تم استلام الطلب',
+  };
+  static const en = {
+    'home': 'Home',
+    'search': 'Search',
+    'categories': 'Categories',
+    'profile': 'Profile',
+    'editYourInfo': 'Edit Your Info',
+    'orders': 'Orders',
+    'mail': 'Mail',
+    'quantity': 'Quantityَ',
+    'piece': 'Piece',
+    'wishlist': 'Wishlist',
+    'emailAlreadyUsed': 'Email already used try another one',
+    'outOfStock': 'Out of stock',
+    'searchHint': 'What are you looking for ?',
+    'emptySearch':
+        'We are not found what you are looking for try another word .',
+    'call': 'Call',
+    'logout': 'Logout',
+    'darkTheme': 'Dark theme',
+    'more_like_this': 'More like this',
+    'customerReviews': 'Customer Reviews',
+    'yourRating': 'Your Rating',
+    'name': 'Name',
+    'email': 'Email',
+    'comment': 'Comment',
+    'submit': 'Submit',
+    'review': 'Review',
+    'masterPiece': 'Masterpiece',
+    'firstName': 'First name',
+    'lastName': 'Last name',
+    'phone': 'Phone',
+    'address1': 'Address1',
+    'address2': 'Address2',
+    'address': 'Address',
+    'addresses': 'Addresses',
+    'favourite': 'Favourite',
+    'settings': 'Settings',
+    'contactUs': 'Contact us',
+    'aboutUs': 'About us',
+    'city': 'Ciry',
+    'state': 'State',
+    'country': 'Country',
+    'company': 'Company',
+    'postCode': 'Post Code',
+    'skip': 'Skip',
+    'splashTitle1': 'Grab best Offers',
+    'splashSubtitle1':
+        'Don\'t miss deals of the day We would like to send you awesome offers every day',
+    'splashTitle2': 'Direct Factory Supply',
+    'splashSubtitle2':
+        'Most of the product are directly supplied from the factory',
+    'splashTitle3': 'Stay up to date products',
+    'splashSubtitle3': 'Get a notification for new products and updates',
+    'splashTitle4': 'Ready to go',
+    'continueToApp': 'Continue',
+    'login': 'Login',
+    'register': 'Register',
+    'forgetPassword': 'Forget your password?',
+    'emailOrUserName': 'Email/UserName',
+    'sendCode': 'Send code',
+    'password': 'password',
+    'resetPassword': 'Reset password',
+    'code': 'Code',
+    'emptyCart': 'السلة فارغة ,ابدا فى اضافة المنتجات الآن',
+    'egp': 'EGP',
+    'removeFromCart': 'Remove from Cart',
+    'cart': 'Cart',
+    'placeOrder': 'Place Order',
+    'payment': 'Payment',
+    'shipping': 'Shipping',
+    'inStock': 'In stock',
+    'requiredValidationMessage': 'This field cannot be empty',
+    'emailValidationMessage': 'This field requires a valid email address',
+    'passwordValidationMessage':
+        'Value must have a length greater than or equal to 6',
+    'lengthTwoValidationMessage':
+        'Value must have a length greater than or equal to 2',
+    'lengthValidationMessage':
+        'Value must have a length greater than or equal to @length',
+    'phoneValidationMessage': 'This field requires a valid phone',
+    'loginAndRegister': 'Login and Register',
+    'chatUs': 'Chat with us',
+    'kwd': 'KWD',
+    'byClicking': 'By clicking you can rate this product',
+    'add': 'Add to cart',
+    'addAddress': 'Add new address ',
+    'productsDetails': 'Products details',
+    'userDetails': 'User details',
+    'orderDetails': 'Order details',
+    'orderDate': 'Order date',
+    'orderStatus': 'Order status',
+    'Hello_world': 'Hello world',
+    'messages_EditedMessage': 'Successfully edited your info.',
+    'messages_logoutMessage': 'Successfully Logged out.',
+    'messages_somethingWrong': 'Something went wrong.',
+    'messages_order': 'Order was made successfully',
+    'messages_cancelOrder': 'Successfully canceled order',
+    'messages_location': 'قم بتحديد عنوانك على الخريطة اولا',
+    'chooseLanguage': 'Choose your language',
+    'english': 'English',
+    'arabic': 'Arabic',
+    'language': 'Language',
+    'appVersion': 'App Version',
+    'total': 'Total',
+    'buyNow': 'Buy now',
+    'chooseAddress': 'Choose Address',
+    'street': 'Street',
+    'buildingNo': 'Building number',
+    'default_one': 'Default',
+    'pickYourLocation': 'Pick your location',
+    'editAddress': 'Edit your address',
+    'selectCountry': 'Select your country',
+    'government': 'Government',
+    'selectGovernment': 'Select your government',
+    'here': 'Here',
+    'orderNumber': 'Order number',
+    'cash': 'Cash on delivery',
+    'emptyCities': 'Not found cities in this country.',
+    'aToZ': 'A-Z',
+    'zToA': 'Z-A',
+    'highToLow': 'High To Low',
+    'lowToHigh': 'Low To High',
+    'flashSale': 'Flash Sale',
+    'topSeller': 'Top Seller',
+    'sortBy': 'Sort by',
+    'mostLiked': 'Most Liked',
+    'special': 'Special',
+    'areYouSure': 'Are you sure',
+    'yes': 'Yes',
+    'no': 'No',
+    'confirm': 'Confirm',
+    'verificationCode': 'Verification code',
+    'weSentSms': 'We\'ve sent an SMS with a verification code to',
+    'resend': 'Resend',
+    'enterCode': 'Enter Code',
+    'invalidCode': 'الكود خاطئ',
+    'weBlockedNumber':
+        'We have blocked all requests from this device due to unusual activity. Try again later',
+    'codeExpired': 'Code is expired',
+    'tooManyRequest':
+        'Too Many Request from this number so we blocked it for now',
+    'invalidPhoneNumber': 'Invalid phone number',
+    'phoneOrEmail': 'Phone or email',
+    'phoneAlreadyUsed': 'This phone already used',
+    'sendingCode': 'Sending code',
+    'rateProduct': 'Rate product now',
+    'ratingBased': 'Based on %s rating',
+    'userReviews': 'User Reviews',
+    'cancelOrder': 'Cancel Order',
+    'orderCanceled': 'A canceled request has been submitted',
+    'orderReturned': 'A return request has been submitted',
+    'returnOrder': 'Return Order',
+    'pendingOrder': 'Order is processing',
+    'orderCompleted': 'Order Completed',
+  };
+}
