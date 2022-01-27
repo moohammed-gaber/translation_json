98ae18836888d2dd42b033bd56d96d137a475548
moohammed-gaber <moohammed.gaber@gmail.com>
2022-01-27 17:15:30 +0200

    l

--- lib/translation_json.dart
+++ lib/translation_json.dart
@@ -1,12 +1,1 @@
 library translation_json;
-
-//hello
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
-//555555444
-//555555444
-
-///5555555555555
